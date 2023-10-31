fc4820ce7b2604e419d95e728e637ff608822263
atul17-11 <84310838+atul17-11@users.noreply.github.com>
2023-10-31 20:30:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ resource "azurerm_resource_group" "atul-rg" {
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.atul-rg.location
+  resource_group_name = azurerm_resource_group.atul-rg.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
@@ -36,8 +36,8 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
   name                  = "webapp-${random_integer.ri.result}"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.atul-rg.location
+  resource_group_name   = azurerm_resource_group.atul-rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
   https_only            = true
   site_config { 
